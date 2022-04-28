a4acb7ea98de23dd392d205c16427513ea33174f
ZoiliSanchez <38706142+ZoilaSanchez@users.noreply.github.com>
2022-04-28 17:38:19 -0600

    Cambios de variables y seguridad en mail

--- helpers/emailOlvidePassword.js
+++ helpers/emailOlvidePassword.js
@@ -4,7 +4,6 @@ const emailOlvidePassword = async (datos) => {
   const transporter = nodemailer.createTransport({
     host: process.env.EMAIL_HOST,
     port: process.env.EMAIL_PORT,
-    secure:false,
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
